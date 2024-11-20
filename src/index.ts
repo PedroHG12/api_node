35179f0b9fb38c20f3830186886256c58f7e983c
lucas-sales-pires <39765462+PedroHG12@users.noreply.github.com>
2024-11-20 17:29:05 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,9 @@ conectarAoBanco()
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+
+app.use(express.urlencoded({ extended: true,  limit: '10mb' }));
 app.use(routerMensagem);
 
 app.listen(porta, () => {
